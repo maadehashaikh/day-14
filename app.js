6dccfa5f55a85305aaa8971f81d019cdaebc27df
Maadeha shaikh <maadeham@gmail.com>
2024-08-23 23:51:11 +0500

    updated !

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
 const express = require('express');
 const app = express();
+const userModel = require('./models/user');
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+app.use(cookieParser());
+
 
 app.get('/',(req,res)=> {
-   res.send("welcome");
+   res.render("index");
+})
+
+app.post('/register',async (req,res)=> {
+  let {username , name , age , email , password} = req.body;
+  let user = await userModel.findOne({email});
+  if(user) return res.status(500).send("user already registered !");
+
+  bcrypt.genSalt(10,(err,salt) =>{
+    bcrypt.hash(password , salt ,async (err,hash) => {
+      let user = await userModel.create({
+        username,
+        name,
+        age,
+        email,
+        password : hash
+      })
+    })
+  })
 })
 
 app.listen(3000);
