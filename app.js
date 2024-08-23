cb689d9aa71b07873994ac5827c01daa8bbe712f
Maadeha shaikh <maadeham@gmail.com>
2024-08-24 01:06:45 +0500

    update

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
-const userModel = require('./models/user');
+const userModel = require('./models/user'); 
+const postModel = require("./models/post")
 const cookieParser = require('cookie-parser');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -12,6 +14,15 @@ app.use(cookieParser());
 
 app.get('/',(req,res)=> {
    res.render("index");
+})
+
+app.get('/login',(req,res)=> {
+  res.render("login");
+})
+
+app.get('/profile', isLoggedIn ,(req,res)=> {
+  console.log(req.user);
+  res.render("login");
 })
 
 app.post('/register',async (req,res)=> {
@@ -27,9 +38,40 @@ app.post('/register',async (req,res)=> {
         age,
         email,
         password : hash
-      })
+      });
+      let token = jwt.sign({email:email , userid: user._id}, "secret");
+      res.cookie("token",token);
+      res.send("You are registered now🤗")
     })
   })
 })
+
+app.post('/login',async (req,res)=> {
+  let {email , password} = req.body;
+  let user = await userModel.findOne({email});
+  if(!user) return res.status(500).send("Something went wrong !"); 
+  bcrypt.compare(password , user.password , function(err,result){
+  if(result) {
+    let token = jwt.sign({email:email , userid: user._id}, "secret");
+      res.cookie("token",token);
+      res.status(200).send("you are login")
+  }
+  else res.redirect("/login")
+  });
+})
+
+app.get('/logout', (req,res)=>{
+  res.cookie("token","");
+  res.redirect("/login")
+})
+
+function isLoggedIn (req , res , next){
+  if(req.cookies.token === "") res.send("You must be logged in")
+  else {
+  let data = jwt.verify(req.cookies.token,"secret");
+  req.user = data ; 
+  }
+  next();
+}
 
 app.listen(3000);
